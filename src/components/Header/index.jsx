aa95eefa37150597258b52d90c98f89eca107a9c
Nurullah-Nergiz <developer@nurullah.aleeas.com>
2023-09-22 22:44:07 +0300

    logo updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,12 @@ import { Avatar } from "./Avatar";
 
 export const Header = () => {
    return (
-      <header className="h-16 flex items-center justify-between">
-         <Link to="/">Logo</Link>
+      <header className="h-16 px-4 flex items-center justify-between shadow-md">
+         <Link to="/">
+            <span className="py-1 px-2 bg-primary text-white rounded-lg">
+               Survey App
+            </span>
+         </Link>
          <Avatar />
       </header>
    );
